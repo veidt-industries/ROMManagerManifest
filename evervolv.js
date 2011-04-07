e230bea8a1523b0251e6ceb2ac23cafb5910cc4b
Will Kline <penguin55@gmail.com>
2011-04-07 14:57:19 -0400

    First copy of the manifest file, not sure where to host this, so github is as good as anything else

---
+++ evervolv.js
@@ -0,0 +1,111 @@
+{
+    "version": "1",
+    "homepage": "http://evervolv.com/",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=PreludeDrew%40gmail%2ecom&lc=US&item_name=ROM%20Development&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted",
+   
+    "roms":
+    [
+    {
+       "name": "Evervolv 1.0.2b6 Acies",
+       "summary": "Development",
+       "device": "supersonic",
+       "product": "Evervolv",
+       "url": "http://mirror.evervolv.com/Acies/Evervolv-Acies-1.0.2b6-Wimacs-supersonic.zip"
+	   "screenshots":
+        [
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/01-launcher.png",
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/05-rotary.png",
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/07-widgets.png",
+			"http://www.evervolv.com/image.php/1?width=112&height=188&image=/screenshots/11-4g.png",
+			"http://www.evervolv.com/image.php/1?width=112&height=188&image=/screenshots/16-ev_settings_lockscreen_carrier.png",
+		]
+		"addons": 
+		[{
+			"name": "Google Apps (latest)",
+			"url": "http://mirror.evervolv.com/GApps/GApps-20110326.zip"
+		},
+		{
+			"name": "Google Apps (older)",
+			"url": 	"http://mirror.evervolv.com/GApps/GApps-20110320.zip"
+		}]
+		
+    },
+   
+   {
+       "device": "speedy",
+	   "name": "Evervolv 1.0.2b1 Artis ",
+       "summary": "development",
+       "product": "Evervolv",
+       "url": "http://mirror.evervolv.com/Artis/Evervolv-Artis-1.0.2b1-Wimacs-speedy.zip"
+	   "screenshots":
+        [
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/01-launcher.png",
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/05-rotary.png",
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/07-widgets.png",
+			"http://www.evervolv.com/image.php/1?width=112&height=188&image=/screenshots/11-4g.png",
+			"http://www.evervolv.com/image.php/1?width=112&height=188&image=/screenshots/16-ev_settings_lockscreen_carrier.png",
+		]
+		"addons": 
+		[{
+			"name": "Google Apps (latest)",
+			"url": "http://mirror.evervolv.com/GApps/GApps-20110326.zip"
+		},
+		{
+			"name": "Google Apps (older)",
+			"url": 	"http://mirror.evervolv.com/GApps/GApps-20110320.zip"
+		}]
+       },
+
+
+    {
+       "device": "inc",
+       "name": "Evervolv 1.0.2b5 Dives",
+       "summary": "Development",
+       "product": "Evervolv",
+       "url": "http://mirror.evervolv.com/Dives/Evervolv-Dives-1.0.2b5-inc.zip"
+	   "screenshots":
+        [
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/01-launcher.png",
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/05-rotary.png",
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/07-widgets.png",
+			"http://www.evervolv.com/image.php/1?width=112&height=188&image=/screenshots/11-4g.png",
+			"http://www.evervolv.com/image.php/1?width=112&height=188&image=/screenshots/16-ev_settings_lockscreen_carrier.png",
+		]
+		"addons": 
+		[{
+			"name": "Google Apps (latest)",
+			"url": "http://mirror.evervolv.com/GApps/GApps-20110326.zip"
+		},
+		{
+			"name": "Google Apps (older)",
+			"url": 	"http://mirror.evervolv.com/GApps/GApps-20110320.zip"
+		}]
+    },
+
+    {
+       "device": "passion",
+       "name": "Evervolv 1.0.2b3 Perdo",
+       "summary": "Development",
+       "product": "Evervolv",
+       "url": "http://mirror.evervolv.com/Perdo/Evervolv-Perdo-1.0.2b3-passion.zip"
+	   "screenshots":
+        [
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/01-launcher.png",
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/05-rotary.png",
+			"http://www.evervolv.com/image.php/0?width=112&height=188&image=/screenshots/07-widgets.png",
+			"http://www.evervolv.com/image.php/1?width=112&height=188&image=/screenshots/11-4g.png",
+			"http://www.evervolv.com/image.php/1?width=112&height=188&image=/screenshots/16-ev_settings_lockscreen_carrier.png",
+		]
+		"addons": 
+		[{
+			"name": "Google Apps (latest)",
+			"url": "http://mirror.evervolv.com/GApps/GApps-20110326.zip"
+		},
+		{
+			"name": "Google Apps (older)",
+			"url": 	"http://mirror.evervolv.com/GApps/GApps-20110320.zip"
+		}]
+    }
+
+    ]
+}
