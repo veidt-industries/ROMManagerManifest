ddce8cfdcb63096acc65da74913f6f6ff1cc16b6
Will Kline <penguin55@gmail.com>
2011-04-08 01:21:07 -0400

    Just the Evervolv part

---
+++ just-evervolv.js
@@ -0,0 +1,13 @@
+{
+		"developer": "Preludedrew",
+		"free": "true",
+		"id": "Evervolv",
+		"manifest": "https://github.com/veidt-industries/ROMManagerManifest/raw/e230bea8a1523b0251e6ceb2ac23cafb5910cc4b/evervolv.js",
+		"roms":  {
+		   "speedy": "true",
+		   "inc": "true",
+		   "supersonic": "true",
+		   "passion": "true"
+		}
+		"summary": "A fast and sexy ROM based off AOSP"
+	},
